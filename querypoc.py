36a80af36ce6ea471c4a173735371e6ab43ad1bd
jrmartin <yeshuarmartinez@gmail.com>
2022-05-16 15:36:14 -0700

    #16 Update script to insert CID_DATA into table

--- querypoc.py
+++ querypoc.py
@@ -1,19 +1,53 @@
 import psycopg2
+import glob
+import os
+import csv
+import time
 
 conn = psycopg2.connect(
     host='localhost',
-    port=54320,
-    dbname='my_database',
+    port=5432,
+    dbname='asu',
     user='postgres',
-    password='my_password',
+    password='password'
 )
 cur = conn.cursor()
-cur.execute("CREATE TABLE t_hash AS SELECT id, md5(id::text) FROM generate_series(1, 50000000) AS id")
-cur.execute("CREATE EXTENSION pg_trgm")
-cur.execute("CREATE INDEX idx_gin ON t_hash USING gin (md5 gin_trgm_ops);")
-cur.execute("SELECT * FROM t_hash WHERE md5 LIKE '%e2345679a%';;")
-result = cur.fetchone()
-print(result)
-conn.commit()
-cur.close()
-conn.close()
+
+try:
+	start = time.time()
+	print("Start:")
+	print(start)	
+	cur.execute("""
+        CREATE TABLE IF NOT EXISTS synonyms (
+            CID SERIAL PRIMARY KEY,
+            Synonym VARCHAR NOT NULL
+        )
+        """)
+
+	# use glob to get all the csv files in the folder
+	path = "CID_Chunks/"
+	csv_files = glob.glob(os.path.join(path, "*.csv"))
+	  
+	sql_insert = """INSERT INTO synonyms(CID, Synonym)
+                VALUES(%s, %s)
+		ON CONFLICT (CID) DO UPDATE SET
+    		(Synonym) = ROW(concat(synonyms.Synonym, ',', EXCLUDED.Synonym))"""
+  
+	# loop over the list of csv files
+	for f in csv_files:
+	    with open(f, 'r') as f1:
+	       reader = csv.reader(f1)
+	       next(reader) # This skips the 1st row which is the header.
+	       for record in reader:
+                  cur.execute(sql_insert, record)
+	conn.commit()
+	cur.close()
+	conn.close()
+	end = time.time()
+	print("End Time")
+	print(end - start)
+except:
+	print("Error")
+	sys.exit()
+
+
