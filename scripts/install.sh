dafe773848fc00a1a4d33af35e231f20837332b7
jrmartin <yeshuarmartinez@gmail.com>
2022-06-26 14:36:41 -0700

    #20 Test

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,10 @@
 wget https://ftp.ncbi.nlm.nih.gov/pubchem/Compound/Extras/CID-Synonym-unfiltered.gz
 gunzip CID-Synonym-unfiltered.gz
 mkdir CID_Chunks
-ls
 python3 scripts/normalize.py
+ls
+cd CID_Chunks
+ls
+cd ..
+ls
 python3 scripts/populateTable.py "/workspace/asu-olfactory/CID_Chunks/"
