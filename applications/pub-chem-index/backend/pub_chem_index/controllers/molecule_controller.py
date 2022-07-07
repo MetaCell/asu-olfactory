f398756f0b9017ac6508d491d2d82eedada6de70
jrmartin <yeshuarmartinez@gmail.com>
2022-07-07 11:55:09 -0700

    #20 - Update rest api to search by CID and synonym

--- applications/pub-chem-index/backend/pub_chem_index/controllers/molecule_controller.py
+++ applications/pub-chem-index/backend/pub_chem_index/controllers/molecule_controller.py
@@ -5,41 +5,26 @@ from pub_chem_index.models.molecule import Molecule  # noqa: E501
 from pub_chem_index import util
 
 
-def create_molecule(molecule):  # noqa: E501
-    """Create a Molecule
-
-    Creates a new instance of a &#x60;Molecule&#x60;. # noqa: E501
-
-    :param molecule: A new &#x60;Molecule&#x60; to be created.
-    :type molecule: dict | bytes
-
-    :rtype: None
-    """
-    if connexion.request.is_json:
-        molecule = Molecule.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
-
-
-def delete_molecule(molecule_id):  # noqa: E501
-    """Delete a Molecule
-
-    Deletes an existing &#x60;Molecule&#x60;. # noqa: E501
-
-    :param molecule_id: A unique identifier for a &#x60;Molecule&#x60;.
-    :type molecule_id: str
-
-    :rtype: None
-    """
-    return 'do some magic!'
-
-
-def get_molecule(molecule_id):  # noqa: E501
+def get_molecule_by_cid(molecule_by_cid):  # noqa: E501
     """Get a Molecule
 
     Gets the details of a single instance of a &#x60;Molecule&#x60;. # noqa: E501
 
-    :param molecule_id: A unique identifier for a &#x60;Molecule&#x60;.
-    :type molecule_id: str
+    :param molecule_by_cid: A unique identifier for a &#x60;Molecule&#x60;.
+    :type molecule_by_cid: str
+
+    :rtype: Molecule
+    """
+    return 'do some magic!'
+
+
+def get_molecule_by_synonym(molecule_by_synonym):  # noqa: E501
+    """Get Molecules by synonym.
+
+    Gets the details of a single instance of a &#x60;Molecule&#x60;. # noqa: E501
+
+    :param molecule_by_synonym: A unique identifier for a &#x60;Molecule&#x60;.
+    :type molecule_by_synonym: str
 
     :rtype: Molecule
     """
@@ -54,21 +39,4 @@ def get_molecules():  # noqa: E501
 
     :rtype: List[Molecule]
     """
-    return 'do some magic!'
-
-
-def update_molecule(molecule_id, molecule):  # noqa: E501
-    """Update a Molecule
-
-    Updates an existing &#x60;Molecule&#x60;. # noqa: E501
-
-    :param molecule_id: A unique identifier for a &#x60;Molecule&#x60;.
-    :type molecule_id: str
-    :param molecule: Updated &#x60;Molecule&#x60; information.
-    :type molecule: dict | bytes
-
-    :rtype: None
-    """
-    if connexion.request.is_json:
-        molecule = Molecule.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
