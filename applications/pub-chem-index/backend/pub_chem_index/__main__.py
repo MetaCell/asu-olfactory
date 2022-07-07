516799712cee5edf7f08badd904865f667928958
Filippo Ledda <filippo@metacell.us>
2022-07-07 16:14:55 +0200

    Deployment fix

--- applications/pub-chem-index/backend/pub_chem_index/__main__.py
+++ applications/pub-chem-index/backend/pub_chem_index/__main__.py
@@ -4,8 +4,10 @@ import connexion
 
 from pub_chem_index import encoder
 
+app = None
 
 def main():
+    global app
     app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml',
