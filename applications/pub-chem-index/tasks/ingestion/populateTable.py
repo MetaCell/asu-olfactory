f1c00789c12ecccde4234dcd46379caef5b2d0ab
jrmartin <yeshuarmartinez@gmail.com>
2022-07-07 09:01:50 -0700

    #20 - Load all csv files

--- applications/pub-chem-index/tasks/ingestion/populateTable.py
+++ applications/pub-chem-index/tasks/ingestion/populateTable.py
@@ -19,29 +19,6 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 try:
-    #Populate a table without GIN Indexing
-    # start = time.time()
-    # cur.execute("""
-    #     CREATE TABLE IF NOT EXISTS synonyms_nongin(
-    #         CID VARCHAR NOT NULL,
-    #         Synonym VARCHAR
-    #     )
-    #     """)
-    # # use glob to get all the csv files in the folder
-    # path = '/CID_Chunks'
-    # csv_files = glob.glob(os.path.join(path, "*.csv"))
-    # # loop over the list of csv files
-    # for f in csv_files:
-    #     sql_copy = '''
-    #         COPY synonyms_nongin
-    #         FROM '%s'
-    #         DELIMITER ',' CSV HEADER;
-    #         ''' % f
-    #     cur.execute(sql_copy)
-
-    # conn.commit()
-    # end = time.time()
-    # print("It took %s seconds to create Non GIN table. ", end - start)
     #Populate GIN indexed table, this will take about 30 minutes.
     start = time.time()
     path = sys.argv[1]
