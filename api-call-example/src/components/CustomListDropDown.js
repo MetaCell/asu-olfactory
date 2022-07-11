0e78869ca184dbfbef4e5565eceeea0e70200591
enicolasgomez <enicolasgomez@gmail.com>
2022-07-11 12:20:32 -0300

    Changed molecules endpoint url on sample app

--- api-call-example/src/components/CustomListDropDown.js
+++ api-call-example/src/components/CustomListDropDown.js
@@ -25,7 +25,7 @@ export default class CustomListDropDown extends React.Component {
     }
   }
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://pubchem.olfactory.dev.metacell.us/molecules')
       .then((response) => response.json())
       .then((res) => this.setState({ collection: res }))
   }
