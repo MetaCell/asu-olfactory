c1a97b5313c3e80500a15af6db1de0567b06ce29
jrmartin <yeshuarmartinez@gmail.com>
2022-08-29 16:55:13 -0700

    #35 - test in main.py controllers are happening

--- applications/pub-chem-index/backend/pub_chem_index/__main__.py
+++ applications/pub-chem-index/backend/pub_chem_index/__main__.py
@@ -1,5 +1,6 @@
 from cloudharness.utils.server import init_flask
 import flask, os
+from flask import redirect
 from pub_chem_index import encoder
 
 app = init_flask(title="Olphactory pubchem index API", webapp=False)
@@ -7,6 +8,10 @@ app = init_flask(title="Olphactory pubchem index API", webapp=False)
 @app.route("/", defaults={"file": "index.html"})
 def index(file):
     return flask.send_from_directory("www", "index.html")
+
+@app.route("/metacell")
+def nn():
+    return redirect("http://www.metacell.us")
 
 def main():
     app.run(host='0.0.0.0', port=8080)
