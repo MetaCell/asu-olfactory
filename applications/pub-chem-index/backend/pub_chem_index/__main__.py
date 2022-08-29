6788d0faeacbf6e08b966d189fbed0276355292c
jrmartin <yeshuarmartinez@gmail.com>
2022-08-29 12:02:33 -0700

    #35 - replace main.py with feature/20

--- applications/pub-chem-index/backend/pub_chem_index/__main__.py
+++ applications/pub-chem-index/backend/pub_chem_index/__main__.py
@@ -1,26 +1,7 @@
-#!/usr/bin/env python3
-import flask
-import connexion
+from cloudharness.utils.server import init_flask, main
 
-from pub_chem_index import encoder
 
-def main():
-    www_path = "static"
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'PubChemIndex'},
-                pythonic_params=True)
-
-    @app.route('/', methods=['GET'])
-    def index():
-        return flask.send_from_directory(www_path, 'index.html')
-
-    @app.route('/<path:path>', methods=['GET'])
-    def send_webapp(path):
-        return flask.send_from_directory(www_path, path)
-
-    app.run(port=8080)
+app = init_flask(title="Olphactory pubchem index API", webapp=False)
 
 if __name__ == '__main__':
     main()
