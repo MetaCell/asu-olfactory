527e98a1a65c750f2b89993947c0c0c41f50ea17
jrmartin <yeshuarmartinez@gmail.com>
2022-08-29 12:21:46 -0700

    #35 - Modify main.py

--- applications/pub-chem-index/backend/pub_chem_index/__main__.py
+++ applications/pub-chem-index/backend/pub_chem_index/__main__.py
@@ -1,7 +1,21 @@
-from cloudharness.utils.server import init_flask, main
-
+from cloudharness.utils.server import init_flask
+import flask, os
+from pub_chem_index import encoder
 
 app = init_flask(title="Olphactory pubchem index API", webapp=False)
+
+def main():
+    www_path = "static"
+
+    @app.route('/', methods=['GET'])
+    def index():
+        return flask.send_from_directory(www_path, 'index.html')
+
+    @app.route('/<path:path>', methods=['GET'])
+    def send_webapp(path):
+        return flask.send_from_directory(www_path, path)
+
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 5001))
 
 if __name__ == '__main__':
     main()
