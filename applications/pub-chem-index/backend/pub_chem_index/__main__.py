ded51e11e1030cedb5fd70b68f88845113de5490
jrmartin <yeshuarmartinez@gmail.com>
2022-08-29 16:02:12 -0700

    #35 - Fix attempt

--- applications/pub-chem-index/backend/pub_chem_index/__main__.py
+++ applications/pub-chem-index/backend/pub_chem_index/__main__.py
@@ -4,18 +4,12 @@ from pub_chem_index import encoder
 
 app = init_flask(title="Olphactory pubchem index API", webapp=False)
 
+@app.route("/", defaults={"file": "index.html"})
+def index(file):
+    return flask.send_from_directory("www", "index.html")
+
 def main():
-    www_path = "/usr/src/app/pub_chem_index/www"
-
-    @app.route('/test-route', methods=['GET'])
-    def index():
-        return flask.send_from_directory(www_path, 'index.html')
-
-    @app.route('/<path:path>', methods=['GET'])
-    def send_webapp(path):
-        return flask.send_from_directory(www_path, path)
-
-    app.run(host='0.0.0.0', port=os.getenv('PORT', 5001))
+    app.run(host='0.0.0.0', port=8080)
 
 if __name__ == '__main__':
     main()
