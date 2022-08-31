c9fab1293bd5bc061df521fe44591bb8c10338cd
jrmartin <yeshuarmartinez@gmail.com>
2022-08-30 17:43:36 -0700

    #35 - fix syntax issue

--- applications/pub-chem-index/backend/pub_chem_index/__main__.py
+++ applications/pub-chem-index/backend/pub_chem_index/__main__.py
@@ -8,17 +8,17 @@ app = init_flask(title="Olphactory pubchem index API", webapp=False)
 @app.route("/", defaults={"file": "index.html"})
 def index(file):
     root_dir = os.path.dirname(os.getcwd())
-    return flask.send_from_directory(os.path.join(root_dir, 'static', "index.html"))
+    return flask.send_from_directory(os.path.join(root_dir, 'static'), "index.html")
 
 @app.route("/test1", defaults={"file": "index.html"})
 def test1(file):
     root_dir1 = os.path.dirname(os.getcwd())
-    return flask.send_from_directory(os.path.join(root_dir1, 'static', "index.html"))
+    return flask.send_from_directory(os.path.join(root_dir1, 'static'), "index.html")
 
 @app.route("/test2", defaults={"file": "index.html"})
 def test2(file):
     root_dir2 = os.path.dirname(os.getcwd())
-    return flask.send_from_directory(os.path.join(root_dir2, 'www', "index.html"))
+    return flask.send_from_directory(os.path.join(root_dir2, 'www'), "index.html")
 
 @app.route("/metacell")
 def nn():
