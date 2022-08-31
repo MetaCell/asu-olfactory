4ecf8808411cda9b7e0f4444fa6a9e18933fe546
jrmartin <yeshuarmartinez@gmail.com>
2022-08-30 17:04:18 -0700

    #35 - Test routes

--- applications/pub-chem-index/backend/pub_chem_index/__main__.py
+++ applications/pub-chem-index/backend/pub_chem_index/__main__.py
@@ -7,11 +7,22 @@ app = init_flask(title="Olphactory pubchem index API", webapp=False)
 
 @app.route("/", defaults={"file": "index.html"})
 def index(file):
-    return flask.send_from_directory("www", "index.html")
+    root_dir = os.path.dirname(os.getcwd())
+    return flask.send_from_directory(os.path.join(root_dir, 'static', "index.html")
+
+@app.route("/test1", defaults={"file": "index.html"})
+def index(file):
+    root_dir = os.path.dirname(os.getcwd())
+    return flask.send_from_directory(os.path.join(root_dir, 'static', "index.html")
+
+@app.route("/test2", defaults={"file": "index.html"})
+def index(file):
+    root_dir = os.path.dirname(os.getcwd())
+    return flask.send_from_directory(os.path.join(root_dir, 'www', "index.html")
 
 @app.route("/metacell")
 def nn():
-    return redirect("http://www.metacell.us")
+    return flask.send_from_directory("www", "index.html")
 
 def main():
     app.run(host='0.0.0.0', port=8080)
