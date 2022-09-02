b2d717cd1dc8d33be2dfd03fbca2a1594b3fc232
enicolasgomez <enicolasgomez@gmail.com>
2022-09-02 10:03:22 -0300

    Changing routing to cloud-harness init_flask (3)

--- applications/pub-chem-index/backend/pub_chem_index/__main__.py
+++ applications/pub-chem-index/backend/pub_chem_index/__main__.py
@@ -3,9 +3,37 @@ import flask, os
 from flask import redirect
 from pub_chem_index import encoder
 
-app = init_flask(title="Olphactory pubchem index API", webapp=True)
+app = init_flask(title="Olphactory pubchem index API", webapp=False)
+
+def init_webapp_routes(www_path):
+    @app.route('/test', methods=['GET'])
+    def test():
+        return 'routing ok'
+
+    @app.route('/', methods=['GET'])
+    def index():
+        return flask.send_from_directory(www_path, 'index.html')
+
+    @app.route('/<path:path>', methods=['GET'])
+    def send_webapp(path):
+        return flask.send_from_directory(www_path, path)
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        # when a 404 is thrown send the "main" index page
+        # unless the first segment of the path is in the exception list
+        first_segment_path = flask.request.full_path.split('/')[1]
+        if first_segment_path in ['api', 'static', 'test']:  # exception list
+            return error
+        return index()
+
+    @app.route('/static/<path:path>', methods=['GET'])
+    def send_static(path):
+        return flask.send_from_directory(os.path.join(www_path, 'static'), path)
 
 def main():
+    www = os.path.dirname(os.path.abspath(__file__)) + "/www"
+    init_webapp_routes(www)
     app.run(host='0.0.0.0', port=8080)
 
 if __name__ == '__main__':
