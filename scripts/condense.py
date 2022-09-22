8f1fe6135d13de750576f2ae54b5114304319b6d
enicolasgomez <enicolasgomez@gmail.com>
2022-09-22 13:00:53 -0300

    Fixed condense script so it works on Dask

--- scripts/condense.py
+++ scripts/condense.py
@@ -3,15 +3,18 @@ import pandas as pd
 import dask.dataframe as dd
 import csv
 
-file = "/Users/infectuz/Downloads/CID/text_index/CID-Synonym-filtered-head"
+file = "/Users/infectuz/Downloads/CID/text_index/CID-Synonym-filtered"
 
-reader = pd.read_csv(file, quoting=csv.QUOTE_NONE, names=['CID-Synonym-filtered'], sep='\t', header=None)
+reader = dd.read_csv(file, quoting=csv.QUOTE_NONE, names=['CID-Synonym-filtered'], sep='\t', header=None)
 output_df = {}
 
 cid_column_indexes = reader.index.unique()
+
 for index in cid_column_indexes:
-  sub_df = reader[reader.index == index]
+  sub_df = reader.loc[index]
   csv_column = sub_df['CID-Synonym-filtered'].str.cat(sep=' ')
   output_df[index] = csv_column
+
+output_df.to_csv("merged-syn.csv")
 print(output_df)
 print("Completed")
