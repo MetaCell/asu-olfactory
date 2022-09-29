05e895aab55a477fd3dc6a6ea8391c532f6c12a7
jrmartin <yeshuarmartinez@gmail.com>
2022-09-29 12:38:46 -0700

    update populate table script #34

--- scripts/populateTable.py
+++ scripts/populateTable.py
@@ -21,43 +21,49 @@ try:
     #Populate a table without GIN Indexing
     start = time.time()
     # use glob to get all the csv files in the folder
-    path = '../CID_Chunks'
+
+    path = "/tmp/CID"
+    logging.info("Populating table using files from %s", path)
 
     file_list = [path + '/' + f for f in os.listdir(path) if f.startswith('CID-')]
 
     for folder in sorted(file_list):
-        logging.info("Folder %s" , folder)
+        logging.info("Reading folder %s", folder)
         file_name = os.path.basename(folder)
-        logging.info("File name %s" ,file_name)
         csv_files = glob.glob(os.path.join(folder, "*.csv"))
 
+        table_name = file_name.replace('-','')
         #Populate GIN indexed table, this will take about 30 minutes.
-        start = time.time()
         sql_copy = """
             CREATE TABLE IF NOT EXISTS %s(
                 CID VARCHAR NOT NULL,
                 Synonym VARCHAR
             )
-            """ % file_name 
+            """ % table_name 
         cur.execute(sql_copy)
-            
-        logging.info("CSV Files %s" ,csv_files)
+        logging.info("Table created")
+        
         # loop over the list of csv files
         for f in csv_files:
-            logging.info("Ingesting file %s" ,f)
+            logging.info("Ingesting file %s", f)
             sql_copy = '''
                 COPY %s
                 FROM '%s'
                 DELIMITER ',' CSV HEADER;
-               '''  % file_name % f
+               '''  % (table_name % f)
+            logging.info("Query is %s", sql_copy)
             cur.execute(sql_copy)
         cur.execute("CREATE EXTENSION IF NOT EXISTS pg_trgm")
-        cur.execute("CREATE INDEX idx_gin ON synonyms USING gin (Synonym gin_trgm_ops);")
-        conn.commit()
-        cur.close()
-        conn.close()
-        end = time.time()
-        logging.info("It took %s seconds to create GIN table. ", end - start)
+        sql_copy = '''CREATE INDEX IF NOT EXISTS idx_gin ON %s USING gin (Synonym gin_trgm_ops);''' % table_name
+        cur.execute(sql_copy)
+        sql_copy = '''CREATE INDEX IF NOT EXISTS cid_idx ON %s (CID);''' % table_name
+        cur.execute(sql_copy)   
 except Exception:
-    traceback.print_exc()
-    print("Error")
+    logging.error("Error during ingestion", exc_info=True)
+    exit(1)
+finally:
+    conn.commit()
+    cur.close()
+    conn.close()
+    end = time.time()
+    logging.info("It took %s seconds to create GIN table. ", end - start)
