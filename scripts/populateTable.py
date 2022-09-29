c01ac1e6544ac8ebfe0027d27fe3912f7ec78767
jrmartin <yeshuarmartinez@gmail.com>
2022-09-29 05:12:03 -0700

    populate table for all folders in CID_CHUNKS #34

--- scripts/populateTable.py
+++ scripts/populateTable.py
@@ -19,56 +19,44 @@ cur = conn.cursor()
 try:
     #Populate a table without GIN Indexing
     start = time.time()
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS synonyms_nongin(
-            CID VARCHAR NOT NULL,
-            Synonym VARCHAR
-        )
-        """)
     # use glob to get all the csv files in the folder
-    path = '/CID_Chunks'
-    csv_files = glob.glob(os.path.join(path, "*.csv"))
-    print(csv_files)
-    # loop over the list of csv files
-    for f in csv_files:
-        print(f)
-        sql_copy = '''
-            COPY synonyms_nongin
-            FROM '%s'
-            DELIMITER ',' CSV HEADER;
-            ''' % f
-        cur.execute(sql_copy)
+    path = '../CID_Chunks'
 
-    conn.commit()
-    end = time.time()
+    file_list = [path + '/' + f for f in os.listdir(path) if f.startswith('CID-')]
 
-    #Populate GIN indexed table, this will take about 30 minutes.
-    start = time.time()
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS synonyms(
-            CID VARCHAR NOT NULL,
-            Synonym VARCHAR
-        )
-        """)
-    # use glob to get all the csv files in the folder
-    csv_files = glob.glob(os.path.join(path, "*.csv"))
-    print(csv_files)
-    # loop over the list of csv files
-    for f in csv_files:
-        print(f)
-        sql_copy = '''
-            COPY synonyms
-            FROM '%s'
-            DELIMITER ',' CSV HEADER;
-            ''' % f
+    for folder in sorted(file_list):
+        print(folder)
+        file_name = os.path.basename(folder)
+        print(file_name)
+        csv_files = glob.glob(os.path.join(folder, "*.csv"))
+
+        #Populate GIN indexed table, this will take about 30 minutes.
+        start = time.time()
+        sql_copy = """
+            CREATE TABLE IF NOT EXISTS '%s'(
+                CID VARCHAR NOT NULL,
+                Synonym VARCHAR
+            )
+            """ % file_name 
         cur.execute(sql_copy)
-    cur.execute("CREATE EXTENSION IF NOT EXISTS pg_trgm")
-    cur.execute("CREATE INDEX idx_gin ON synonyms USING gin (Synonym gin_trgm_ops);")
-    conn.commit()
-    cur.close()
-    conn.close()
-    end = time.time()
-    print("It took %s seconds to create GIN table. ", end - start)
+            
+        print(csv_files)
+        # loop over the list of csv files
+        for f in csv_files:
+            print(f)
+            sql_copy = '''
+                COPY '%s'
+                FROM '%s'
+                DELIMITER ',' CSV HEADER;
+               '''  % file_name % f
+            cur.execute(sql_copy)
+        cur.execute("CREATE EXTENSION IF NOT EXISTS pg_trgm")
+        cur.execute("CREATE INDEX idx_gin ON synonyms USING gin (Synonym gin_trgm_ops);")
+        conn.commit()
+        cur.close()
+        conn.close()
+        end = time.time()
+        print("It took %s seconds to create GIN table. ", end - start)
 except Exception:
     traceback.print_exc()
     print("Error")
