c910c48d855168f45bfb452f053f3566d5b7a90e
jrmartin <yeshuarmartinez@gmail.com>
2022-09-29 09:26:27 -0700

    Test root path for downloads #34

--- applications/pub-chem-index/tasks/ingestion/ingest.sh
+++ applications/pub-chem-index/tasks/ingestion/ingest.sh
@@ -1,7 +1,5 @@
 ls -la /data/db/
 cd /data/db
-mkdir files
-cd files
 [ -d "CID-Synonym-unfiltered" ] && echo "CID files exist. Skipping download" || wget -q -nc https://ftp.ncbi.nlm.nih.gov/pubchem/Compound/Extras/CID-Synonym-unfiltered.gz && gunzip CID-Synonym-unfiltered.gz
 [ -d "CID-Synonym-filtered" ] && echo "CID files exist. Skipping download" || wget -q -nc https://ftp.ncbi.nlm.nih.gov/pubchem/Compound/Extras/CID-Synonym-filtered.gz && gunzip CID-Synonym-filtered.gz
 [ -d "CID-IUPAC" ] && echo "CID files exist. Skipping download" || wget -q -nc https://ftp.ncbi.nlm.nih.gov/pubchem/Compound/Extras/CID-IUPAC.gz && gunzip CID-IUPAC.gz
@@ -13,5 +11,5 @@ cd files
 ls -la /data/db/files
 
 
-python3 /normalize.py /data/db/files /data/db/CID_Chunks
+python3 /normalize.py /data/db /data/db/CID_Chunks
 python3 /populateTable.py /data/db/CID_Chunks
