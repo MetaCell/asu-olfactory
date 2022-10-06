022709e85727295593e5a767cf033d96834b25be
jrmartin <yeshuarmartinez@gmail.com>
2022-10-06 10:31:58 -0700

    Test async psql requests and test with synonyms too #40

--- applications/pub-chem-index/tasks/ingestion/ingest.sh
+++ applications/pub-chem-index/tasks/ingestion/ingest.sh
@@ -1,11 +1,9 @@
 pwd
-ls -ls
 ls -la /data/db/
 cd /data/db/
-ls -ls
+[ -d "CID-Synonym-filtered" ] && echo "CID files exist. Skipping download" || wget -q -nc https://ftp.ncbi.nlm.nih.gov/pubchem/Compound/Extras/CID-Synonym-filtered.gz && gunzip CID-Synonym-filtered.gz
 [ -d "CID-MeSH" ] && echo "CID files exist. Skipping download" || wget -q -nc https://ftp.ncbi.nlm.nih.gov/pubchem/Compound/Extras/CID-MeSH
-
-ls -la /data/db/CID-MeSH
+ls -la /data/db
 pwd
 
 python3 /populate_parallel.py /data/db /data/db/CID
--- applications/pub-chem-index/tasks/ingestion/populate_parallel.py
+++ applications/pub-chem-index/tasks/ingestion/populate_parallel.py
@@ -88,6 +88,7 @@ async def populate_table(table_name, path, dns):
   # loop over the list of csv files
   file_list = [path + f for f in os.listdir(path) if f.startswith('export-')]
 
+  sql_list = []
   for f in file_list:
     logging.info("Ingesting file %s", f)
     sql_copy = '''
@@ -96,7 +97,9 @@ async def populate_table(table_name, path, dns):
         DELIMITER '\t' CSV HEADER;
         '''  % (table_name , f)
     logging.info("Query is %s", sql_copy)
-    await execute_sql(pool, sql_copy)    
+    sql_list.append(sql_copy)
+    
+  await asyncio.gather(*[execute_sql(pool, sql_list[i]) for i in range(len(sql_list))])
 
   await execute_sql(pool, "CREATE EXTENSION IF NOT EXISTS pg_trgm")
   sql_copy = '''CREATE INDEX IF NOT EXISTS idx_gin ON %s USING gin (%s gin_trgm_ops);''' % (table_name, main_column)
