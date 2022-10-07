0b28a670332a790e765d3cf332b3be19a16c8c73
jrmartin <yeshuarmartinez@gmail.com>
2022-10-07 06:20:50 -0700

    set proper pod affinity

--- applications/pub-chem-index/backend/pub_chem_index/controllers/default_controller.py
+++ applications/pub-chem-index/backend/pub_chem_index/controllers/default_controller.py
@@ -13,7 +13,7 @@ def ingest():  # noqa: E501
     :rtype: str
     """
     shared_directory = 'pubchem-db:/data/db'
-    task_ingest = tasks.CustomTask('ingest', 'pub-chem-index-ingestion', shared_directory=shared_directory)
+    task_ingest = tasks.CustomTask('ingest', 'pub-chem-index-ingestion', shared_directory=shared_directory, pod_context=operations.PodExecutionContext(key='app', value='pubchem-db', required=True))
 
     op = operations.PipelineOperation(
         'ingest-data-', (task_ingest, ), shared_directory=shared_directory)
