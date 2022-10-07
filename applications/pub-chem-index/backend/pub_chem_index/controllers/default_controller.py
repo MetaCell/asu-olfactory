355ebd39e474528e52407319f3f13a2220d3e893
jrmartin <yeshuarmartinez@gmail.com>
2022-10-07 10:11:45 -0700

    add affinity to operation #40

--- applications/pub-chem-index/backend/pub_chem_index/controllers/default_controller.py
+++ applications/pub-chem-index/backend/pub_chem_index/controllers/default_controller.py
@@ -13,9 +13,9 @@ def ingest():  # noqa: E501
     :rtype: str
     """
     shared_directory = 'pubchem-db:/data/db'
-    task_ingest = tasks.CustomTask('ingest', 'pub-chem-index-ingestion', shared_directory=shared_directory, pod_context=operations.PodExecutionContext(key='app', value='pubchem-db', required=True))
+    task_ingest = tasks.CustomTask('ingest', 'pub-chem-index-ingestion', shared_directory=shared_directory)
 
     op = operations.PipelineOperation(
-        'ingest-data-', (task_ingest, ), shared_directory=shared_directory)
+        'ingest-data-', (task_ingest, ), shared_directory=shared_directory, pod_context=operations.PodExecutionContext(key='app', value='pubchem-db', required=True))
     op.execute()
     return "Ingesting Data"
