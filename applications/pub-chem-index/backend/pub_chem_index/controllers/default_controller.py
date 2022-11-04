63a673ecf33e24c288a1da61aa74ddde305bd725
jrmartin <yeshuarmartinez@gmail.com>
2022-11-03 22:24:17 -0700

    Revert default_controller after api regeneration #33

--- applications/pub-chem-index/backend/pub_chem_index/controllers/default_controller.py
+++ applications/pub-chem-index/backend/pub_chem_index/controllers/default_controller.py
@@ -1,8 +1,24 @@
 import connexion
 import six
-
+from cloudharness.workflows import tasks, operations
 from pub_chem_index import util
 
+INGESTION_FILES = [
+    "CID-MeSH",
+    "CID-Synonym-filtered.gz",
+    "CID-IUPAC.gz",
+    "CID-InChI-Key.gz",
+    "CID-Title.gz",
+    "CID-SMILES.gz",
+    "CID-Synonym-unfiltered.gz",
+    "CID-Component.gz",
+    "CID-Parent.gz",
+    "CID-Patent.gz",
+    "CID-PMID.gz",
+    "CID-Mass.gz",
+    "CID-PMID.gz",
+    "CID-SID.gz"
+]
 
 def ingest():  # noqa: E501
     """ingest
@@ -12,4 +28,28 @@ def ingest():  # noqa: E501
 
     :rtype: str
     """
-    return 'do some magic!'
+    shared_directory = '/data/db'
+    ingestion_tasks = []
+    for ingestion_file in INGESTION_FILES:
+        ingestion_tasks.append(
+            tasks.CustomTask(
+                f'ingest-{ingestion_file.lower().split(".")[0]}',
+                'pub-chem-index-ingestion',
+                shared_directory=shared_directory,
+                filename=f"{ingestion_file}"
+            )
+        )
+
+    # to ingest all files sequential replace the parallel op with a pipeline op
+    # op = operations.PipelineOperation(
+
+    # ingest all files parallel
+    op = operations.ParallelOperation(
+        f'ingest-data', tuple(ingestion_tasks),
+        shared_directory=shared_directory,
+        shared_volume_size=64000 # 64Gi
+    )
+    op.execute()
+    return "Ingesting Data"
+
+#
