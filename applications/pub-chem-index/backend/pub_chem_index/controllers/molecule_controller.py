9f7d2d6bc61897de2085f8bb616bd6de30fdaf46
jrmartin <yeshuarmartinez@gmail.com>
2022-11-14 19:34:09 -0800

    Fixing join results #23

--- applications/pub-chem-index/backend/pub_chem_index/controllers/molecule_controller.py
+++ applications/pub-chem-index/backend/pub_chem_index/controllers/molecule_controller.py
@@ -420,14 +420,15 @@ def join_results(table_name, column_name, term, properties):
     for i, t in enumerate(first_results):
         result = {}
         result["cid"] = t[0]
-        result[column_name] = t[0]
+        result[column_name] = t[1]
         logging.info("Looking at %s", t[0])
         for table in tables_list:
             table_results = lookup.search_table_by_cid(table, int(t[0]))
             logging.info("Looking at table %s", table)
             for t in enumerate(table_results):
-                result[table] = t[1]
-        results.append(result)
+                result[table] = t[1][1]
+            results.append(result)
+            logging.info("Looking at result %s", result)
 
     return results
 
